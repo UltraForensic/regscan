60ca1a0a8a8aa5dac752f1fc6d138ced237c54a1
UltraForensic <s4bur0@yahoo.co.jp>
2024-07-18 10:40:54 +0900

    Update taskcache scanner to collect additional information from Tasks tree

--- src/scanner/software/taskcache.rs
+++ src/scanner/software/taskcache.rs
@@ -11,6 +11,48 @@ pub fn scan(parser: &mut Parser, target: &String) ->  Option<String> {
 
                     for skey in sub_keys {
                         let last_key_write_timestamp = get_date_time_from_filetime(skey.detail.last_key_written_date_and_time());
+                        match skey.get_value("Id") {
+                            Some(v) => {
+                                match parser.get_key(&format!("Microsoft\\Windows NT\\CurrentVersion\\Schedule\\TaskCache\\Tasks\\{}", v.get_content().0), false) {
+                                    Ok(r2) => {
+                                        match r2 {
+                                            Some(key2) => {
+                                                let mut result_str = format!("taskcache\tTaskCache {} found (", skey.key_name);
+                                                match key2.get_value("Path") {
+                                                    Some(path) => {
+                                                        result_str = format!("{}path = \"{}\", ", result_str, path.get_content().0);
+                                                    },
+                                                    None => {}
+                                                }
+                                                match key2.get_value("Date") {
+                                                    Some(path) => {
+                                                        result_str = format!("{}Date = \"{}\", ", result_str, path.get_content().0);
+                                                    },
+                                                    None => {}
+                                                }
+                                                match key2.get_value("Author") {
+                                                    Some(path) => {
+                                                        result_str = format!("{}Author = \"{}\", ", result_str, path.get_content().0);
+                                                    },
+                                                    None => {}
+                                                }
+                                                match key2.get_value("URI") {
+                                                    Some(path) => {
+                                                        result_str = format!("{}URI = \"{}\"", result_str, path.get_content().0);
+                                                    },
+                                                    None => {}
+                                                }
+                                                results.push(format!("{})\t{}\t{}\\{}\t{}", result_str, target, key_path, skey.key_name, last_key_write_timestamp));
+                                                continue;
+                                            },
+                                            None => {}
+                                        }
+                                    },
+                                    Err(_e) => {}
+                                }
+                            },
+                            None => {}
+                        }
                         results.push(format!("taskcache\tTaskCache {} found\t{}\t{}\\{}\t{}", skey.key_name, target, key_path, skey.key_name, last_key_write_timestamp));
                     }
 
